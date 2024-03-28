7d64c56b950ed24fd7e02a316a0973b9c0a8718c
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:18:26 +0500

    Add files via upload

---
+++ ques9.js
@@ -0,0 +1,4 @@
+"use strict";
+//  9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let myFavNum = 6;
+console.log(`My favourite number is ${myFavNum}`);
